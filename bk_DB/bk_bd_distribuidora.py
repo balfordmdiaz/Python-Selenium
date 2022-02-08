69c490da925c54bade1960fdd53fd2fa515d8711
unknown <balfordm21@gmail.com>
2022-02-08 15:16:12 -0600

    Modificacion de bk_distribuidora

--- bk_DB/bk_bd_distribuidora.py
+++ bk_DB/bk_bd_distribuidora.py
@@ -2,6 +2,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 class bk_database(unittest.TestCase):
@@ -11,22 +13,26 @@ class bk_database(unittest.TestCase):
         chromeOptions.add_experimental_option("prefs", {
         "download.default_directory" : "C:\\Users\\Balford\\OneDrive\\Documents\\Distribuidora",
         })
-        self.driver = webdriver.Chrome(executable_path=r"C:\driverChrome\chromedriver.exe", chrome_options=chromeOptions)
-
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chromeOptions)
 
     def test_search(self):
         driver = self.driver
-        driver.get("https://distri21-cp5038.wordpresstemporal.com:2083/cpsess0604349005/3rdparty/phpMyAdmin/db_structure.php?server=1&db=distri21_distribuidorahd")
-        user = driver.find_element_by_id("user")   #utilizando el campo id
+        driver.get("https://cp7048.webempresa.eu:2443/login/")
+        time.sleep(3)
+        user = driver.find_element(By.ID, "login")   #utilizando el campo id
         user.send_keys("distri21")  #ingresando los valores al campo
-        passw = driver.find_element_by_id("pass")
+        passw = driver.find_element(By.ID, "password")
         passw.send_keys("9a4(JF#Zr9Bar2")
         passw.send_keys(Keys.ENTER)
+        time.sleep(3)
+        driver.get("https://cp7048.webempresa.eu:2443/phpMyAdmin/") #dirigir a base de datos
+        time.sleep(3)
+        submenu = driver.find_element(By.XPATH, "//*[@id='topmenucontainer']/nav/button/span").click()
         time.sleep(1)
-        export = driver.find_element_by_xpath("//*[@id='topmenu']/li[5]/a")
+        export = driver.find_element(By.XPATH, "//*[@id='topmenu']/li[4]/a")
         export.click()
         time.sleep(1)
-        download = driver.find_element_by_xpath("/html/body/div[4]/form/div[7]/input").click()
+        download = driver.find_element(By.ID, "buttonGo").click()
         time.sleep(3)
 
     def tearDown(self): #cerramos el driver
